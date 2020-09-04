a32262368cc3971f93b2d58f44bf66d590f04cc7
Mike <mad3mike@gmail.com>
2020-09-04 13:33:30 +0300

    Signed-off-by: Mike <mad3mike@gmail.com>

---
+++ Complex.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonGB3
+{
+    struct Complex
+    {
+
+        private double a, b;
+
+        //public void SetA(double value)
+        //{
+        //    a = value;
+        //}
+        //public double GetA (){ return a; }
+
+        //public void SetB(double value)
+        //{
+        //    b = value;
+        //}
+
+        //public double GetB() { return b; }
+
+
+        public double B 
+        {
+            get {return b; }
+                set { b = value; }
+        }
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        static public Complex Sum(Complex x, Complex y)
+        {
+            return new Complex(Re: x.a + y.a,
+                               Im: x.b + y.b);
+
+
+        }
+        static public Complex Dif(Complex x, Complex y)
+        {
+            return new Complex(Re: x.a - y.a,
+                               Im: x.b - y.b);
+                
+
+        }
+        static public Complex Mult(Complex x, Complex y)
+        {
+            return new Complex(Re: x.a * y.a - x.b*y.b,
+                               Im: x.a*y.b + y.a*x.b);
+        }
+        public Complex(double Re, double Im) 
+        {
+          a = Re;
+          b = Im;
+        }
+
+        public override string ToString()
+        {
+            if (b == 0) return $"{a}";
+            else if (a == 0) return $"{b}i";
+            else if (B == -1) return $"{a} - i";
+            else if (B == 1) return $"{a} + i";
+            return (b < 0) ? $"{a} - {-b}i": $"{a} +{b}i";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LessonGB3
+{
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Задание 1");
+            Console.WriteLine("");
+            Ex1Complex();
+            Console.WriteLine("");
+            Console.WriteLine("Задание 2");
+            Console.WriteLine("");
+
+            Exer2();
+
+
+
+            Console.ReadLine();
+        }
+        static void Ex1Complex()
+        {
+            Complex z1 = new Complex();
+            z1.A = 2;
+            z1.B = 3;
+            Console.WriteLine(z1);
+
+
+            Complex z2 = new Complex(Re: 0, Im: -20);
+            z2.A = 2;
+            z2.B = 1;
+
+            Console.WriteLine(z2);
+
+            Complex z3 = Complex.Sum(z1, z2);
+            Console.WriteLine(z3);
+            Complex z4 = Complex.Dif(z1, z2);
+            Console.WriteLine(z4);
+            Complex z5 = Complex.Mult(z1, z2);
+            Console.WriteLine(z5);
+
+        
+        }
+        static void Exer2()
+        {
+
+            int sum = 0;
+            string z = "";
+            int AmountOfOddNumbers = 0;
+            Ex2();
+
+             void Ex2() {
+                
+                Console.WriteLine("Введите число от 1 до 9");
+                string s = Console.ReadLine();
+                int n;
+                if (!int.TryParse(s, out n))
+                {
+                    Console.WriteLine("Ошибка! Вы должны ввести число!");
+                }
+
+
+                else if (n % 2 != 0 && AmountOfOddNumbers == 0)
+                {
+                    
+                    sum += n;
+                    z += $"{n}";
+                    AmountOfOddNumbers++;
+                }
+                else if (n % 2 != 0 && AmountOfOddNumbers > 0)
+                {
+
+                    sum += n;
+                    z += $"+{n}";
+                    AmountOfOddNumbers++;
+                }
+
+
+                else if (n == 0)
+                {
+                    z += $"={sum}";
+                    Console.WriteLine(z);
+                    Console.WriteLine("Сумма нечетных чисел = " + sum);
+
+                    sum = 0;
+                    z = "";
+                    AmountOfOddNumbers = 0;
+
+                }
+                Ex2();
+
+            }
+
+            }
+        }
+
+    }
+
